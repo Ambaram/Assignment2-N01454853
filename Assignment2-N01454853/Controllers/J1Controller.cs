3baded489c9b1634fe9ebde723c10634c528bf73
ambaram <ambaram@ambarams-MacBook-Air.local>
2021-03-03 13:50:28 +0530

    initial commit

---
+++ Assignment2-N01454853/Controllers/J1Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment2_N01454853.Controllers
+{
+    [ApiController]
+    [Route("api/J1/Menu/{Burger}/{Drink}/{Side}/{Dessert}")]
+    public class J1Controller : Controller
+    {
+        /// <summary>
+        /// This code computes the total calorie value of the meal.
+        /// Menu contains burgers,drinks,side orders,dessert.
+        /// Input provided in form of digit choice.
+        /// </summary>
+        /// <param name="Burger">Burger Choice</param>
+        /// <param name="Drink">Drink Choice</param>
+        /// <param name="Side">Side Order Choice</param>
+        /// <param name="Dessert">Dessert Choice</param>
+        /// <returns>This code returns the total calorie count of the meal.</returns>
+        [HttpGet]
+        public string Menu(int Burger, int Drink, int Side, int Dessert)
+        {
+            Dictionary<string, int> Burgers =
+              new Dictionary<string, int>();
+            Burgers.Add("CheeseBurger", 461);
+            Burgers.Add("Fish Burger", 431);
+            Burgers.Add("Veggie Burger", 420);
+            Burgers.Add("No Burger", 0);
+
+            Dictionary<string, int> Drinks =
+              new Dictionary<string, int>();
+            Drinks.Add("Soft Drink", 130);
+            Drinks.Add("Orange Juice", 160);
+            Drinks.Add("Milk", 118);
+            Drinks.Add("no drink", 0);
+
+            Dictionary<string, int> Sides =
+              new Dictionary<string, int>();
+            Sides.Add("Fries", 100);
+            Sides.Add("Baked Potato", 57);
+            Sides.Add("Chef Salad", 70);
+            Sides.Add("no side order", 0);
+
+            Dictionary<string, int> Desserts =
+              new Dictionary<string, int>();
+            Desserts.Add("Apple Pie", 167);
+            Desserts.Add("Sundae", 266);
+            Desserts.Add("Fruit Cup", 75);
+            Desserts.Add("No Dessert", 0);
+
+            int sum = Burgers.ElementAt(Burger-1).Value + Drinks.ElementAt(Drink-1).Value
+              + Sides.ElementAt(Side-1).Value + Desserts.ElementAt(Dessert-1).Value;
+
+            return ("Your total calorie count is "+ sum);
+        }
+    }
+}
---
+++ Assignment2-N01454853/Controllers/J2Controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace zxczx.Controllers
+{
+    [ApiController]
+    [Route("api/J2/DiceGame/{m}/{n}")]
+    public class J2Controller : Controller
+    {
+        /// <summary>
+        /// This code computes number of ways in which two dices can be rolled to obtain a total of 10.
+        /// each dice has variable sides.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// GET api/J2/DiceGame/6/8
+        /// GET api/J2/DiceGame/12/4
+        /// GET api/J2/DiceGame/3/3
+        /// GET api/J2/DiceGame/5/5
+        /// <returns>Number of ways in which the dice can be rolled to obtain a total of 10</returns>
+        [HttpGet]
+        public string DiceGame(int m, int n)
+        {
+            int[] arr1 = new int[m];
+            int[] arr2 = new int[n];
+            int i;
+            int j;
+            // variable initialization which will return the total number of ways.
+            int k = 0;
+            for (i = 0; i < m; i++)
+            {
+                arr1[i] = i + 1;
+            }
+            for (j = 0; j < n; j++)
+            {
+                arr2[j] = j + 1;
+            }
+
+            foreach (int a in arr1)
+            {
+                foreach (int b in arr2)
+                {
+                    if (a + b == 10)
+                    {
+                        k = k + 1;
+                    }
+                }
+            }
+            return ("There are total " + k.ToString() + " ways to get the sum 10");
+        }
+    }
+} 
